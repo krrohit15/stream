3cd2934f469fe99cd5fe3268df9786d0e4bbd258
rohit <rkbarahpuria@gmail.com>
2021-11-06 06:37:19 +0530

    java8 stream setup

---
+++ java8/src/main/java/com/rohit/learn/java8/App.java
@@ -0,0 +1,13 @@
+package com.rohit.learn.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! java8 setup" );
+    }
+}
